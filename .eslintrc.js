20e7db3e52f1ed21e5ba8b4ad79c5c6c08fdeb0e
jmejias08 <josuemejiasvillalobos@gmail.com>
2025-07-02 23:42:43 -0600

    first test in render 4

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,16 @@
+/** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: ['next', 'next/core-web-vitals', 'plugin:@typescript-eslint/recommended'],
+  extends: [
+    'next',
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  plugins: ['@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
   rules: {
     '@typescript-eslint/no-explicit-any': 'off',
-  },
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off'
+  }
 };
